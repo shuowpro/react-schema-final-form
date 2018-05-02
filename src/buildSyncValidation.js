bfa8a35dee4af32c4e9b23af82352ee9bde6a90d
Shuo WANG <shuowang@m-C02VJ028HV2Q.us.alcatel-lucent.com>
2018-05-02 15:53:37 -0400

    remove console.logs

--- src/buildSyncValidation.js
+++ src/buildSyncValidation.js
@@ -4,7 +4,6 @@ import Ajv from 'ajv';
 const setError = (error, schema, res) => {
   // remove the first '.', convert '.foo.bar' to 'foo.bar'
   const dataPath = error.dataPath.charAt(0) === '.' ? error.dataPath.slice(1) : error.dataPath;
-  console.log(error.dataPath);
   _set(res, dataPath, error.message);
   return res;
 };
@@ -20,7 +19,6 @@ const buildSyncValidation = (schema) => {
       return {};
     }
     const errors = ajv.errors.reduce((res, error) => setError(error, schema, res), {});
-    console.log(errors);
     return errors;
   }
 }
